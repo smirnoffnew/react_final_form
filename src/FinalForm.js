b59494d57e7c044d0826182c30af9c57fea392e5
smirnoffnew <smirnoffnew@gmail.com>
2019-08-09 17:59:08 +0300

    added reset after validation

--- src/FinalForm.js
+++ src/FinalForm.js
@@ -18,242 +18,245 @@ const FinalForm = () => (
     <Form 
         onSubmit={showResults}
         decorators={[focustOnError]}
+        initialValues={{ employed: true }}
         subscription={{
             submitting: true
         }}
-    >
-       
 
-        {({ handleSubmit, submitting }) => (
+        render={({submitting, handleSubmit, form}) => (
 
-
-        <form 
-            style = {{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignItems: 'center'
-            }}
-
-            onSubmit = { event => {
-                    handleSubmit(event)
-            }}
-        >
-
-                <div> 
-
-                <Field 
-                    name='firstName' 
-                    placeholder='First Name'
-                    validate={ required }
-                    subscription={{
-                        value: true,
-                        active: true,
-                        error: true,
-                        touched: true
-                    }}
-                >
-                    {
-                        ({input, meta, placeholder}) => (
-
-                            <div 
-                                style={{ 
-                                    position: 'relative',
-                                    display: 'flex', 
-                                    flexDirection: 'column', 
-                                    alignItems: 'center' ,
-                                    background: meta.active ? '#ccffff' : 'transparent'
-                                }}
-                            >
-
-                                <TextField 
-                                    {...input} 
-                                    placeholder={placeholder}  
-                                    label="First Name"
-                                    variant='outlined'
-                                    margin='dense'
-                                />
-
-                                {meta.error && meta.touched && 
-
-                                    <span 
-                                        style={{ 
-                                            position: 'absolute',
-                                            right: '-40%',
-                                            top: '30%',
-                                            color: "red", 
-                                            padding: '0 10px' 
-                                        }}  
-                                    >
-                                        <Typography variant='body1'>
-                                            {meta.error}
-                                        </Typography>
-                                    </span>
-
-                                }
-
-                            </div>
-                        )
-                    }
-
-                </Field>
-
-            </div>
-
-            <div> 
-
-                <Field 
-                    name='secondName' 
-                    placeholder='Second Name'
-                    validate={ required }
-                    subscription={{
-                        value: true,
-                        active: true,
-                        error: true,
-                        touched: true
-                    }}
-                >
-                    {
-                        ({input, meta, placeholder}) => (
-
-                            <div 
-                                style={{ 
-                                    position: 'relative',
-                                    display: 'flex', 
-                                    flexDirection: 'column', 
-                                    alignItems: 'center',
-                                    background: meta.active ? '#ccffff' : 'transparent'
-                                }}
-                            >
-
-                                <TextField 
-                                    {...input} 
-                                    placeholder={placeholder}  
-                                    label="Second Name"
-                                    variant='outlined'
-                                    margin='dense'
-                                />
-
-                                {meta.error && meta.touched &&
-
-                                    <span 
-                                        style={{ 
-                                            position: 'absolute',
-                                            right: '-40%',
-                                            top: '30%',
-                                            color: "red", 
-                                            padding: '0 10px' 
-                                        }} 
-                                    >
-                                        <Typography variant='body1'>
-                                            {meta.error}
-                                        </Typography>
-                                    </span>
-
-                                }
-
-                            </div>
-
-                        )
-                    }
-
-                </Field>
-
-            </div>
-
-            <div> 
-
-                <Field 
-                    name='email' 
-                    placeholder='Email'
-                    validate={ required }
-                    subscription={{
-                        value: true,
-                        active: true,
-                        error: true,
-                        touched: true
-                    }}
-                >
-                    {
-                        ({input, meta, placeholder}) => (
-
-                            <div 
-                                style={{ 
-                                    position: 'relative',
-                                    display: 'flex', 
-                                    flexDirection: 'column', 
-                                    alignItems: 'center',
-                                    background: meta.active ? '#ccffff' : 'transparent' 
-                                }}
-                            >
-
-                                <TextField 
-                                    {...input} 
-                                    placeholder={placeholder}  
-                                    label="Email"
-                                    type="email"
-                                    variant='outlined'
-                                    margin='dense'
-                                />
-
-                                {meta.error && meta.touched &&
-                                    <span 
-                                        style={{ 
-                                            position: 'absolute',
-                                            right: '-40%',
-                                            top: '30%',
-                                            color: "red", 
-                                            padding: '0 10px' 
-                                        }} 
-                                    >
-                                        <Typography variant='body1'>
-                                            {meta.error}
-                                        </Typography>
-                                    </span>
-                                }
-
-                            </div>
-                        )
-                    }
-
-                </Field>
-
-            </div>
-            
-            
-            <Button 
-                type='submit'
-                variant='contained'
-                color='primary'
-                disabled={submitting}
+            <form
                 style={{
-                    margin: '10px 0 10px 0'
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                }}
+
+                onSubmit={ async (event) => {
+                    await handleSubmit(event)
+                    form.reset()
                 }}
             >
-                Submit
-            </Button>
-            
-            <FormSpy subscription={{ values: true}}>
 
-                {({values}) => 
-                    <pre 
-                        style={{
-                            minHeight:'5rem',
-                            width: '100%',
-                            background: 'lightgrey',
-                            border: '5px solid grey'
+                <div>
+
+                    <Field
+                        name='firstName'
+                        placeholder='First Name'
+                        validate={required}
+                        subscription={{
+                            value: true,
+                            active: true,
+                            error: true,
+                            touched: true
                         }}
-                    > 
-                        {JSON.stringify(values, undefined, 2)} 
-                    </pre>
-                }
+                    >
+                        {
+                            ({ input, meta, placeholder }) => (
 
-            </FormSpy>
+                                <div
+                                    style={{
+                                        position: 'relative',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        background: meta.active ? '#ccffff' : 'transparent'
+                                    }}
+                                >
 
-        </form>
-    )}
+                                    <TextField
+                                        {...input}
+                                        placeholder={placeholder}
+                                        label="First Name"
+                                        variant='outlined'
+                                        margin='dense'
+                                        required
+                                    />
 
-    </Form>
+                                    {meta.error && meta.touched &&
+
+                                        <span
+                                            style={{
+                                                position: 'absolute',
+                                                right: '-40%',
+                                                top: '30%',
+                                                color: "red",
+                                                padding: '0 10px'
+                                            }}
+                                        >
+                                            <Typography variant='body1'>
+                                                {meta.error}
+                                            </Typography>
+                                        </span>
+
+                                    }
+
+                                </div>
+                            )
+                        }
+
+                    </Field>
+
+                </div>
+
+                <div>
+
+                    <Field
+                        name='secondName'
+                        placeholder='Second Name'
+                        validate={required}
+                        subscription={{
+                            value: true,
+                            active: true,
+                            error: true,
+                            touched: true
+                        }}
+                    >
+                        {
+                            ({ input, meta, placeholder }) => (
+
+                                <div
+                                    style={{
+                                        position: 'relative',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        background: meta.active ? '#ccffff' : 'transparent'
+                                    }}
+                                >
+
+                                    <TextField
+                                        {...input}
+                                        placeholder={placeholder}
+                                        label="Second Name"
+                                        variant='outlined'
+                                        margin='dense'
+                                        required
+                                    />
+
+                                    {meta.error && meta.touched &&
+
+                                        <span
+                                            style={{
+                                                position: 'absolute',
+                                                right: '-40%',
+                                                top: '30%',
+                                                color: "red",
+                                                padding: '0 10px'
+                                            }}
+                                        >
+                                            <Typography variant='body1'>
+                                                {meta.error}
+                                            </Typography>
+                                        </span>
+
+                                    }
+
+                                </div>
+
+                            )
+                        }
+
+                    </Field>
+
+                </div>
+
+                <div>
+
+                    <Field
+                        name='email'
+                        placeholder='Email'
+                        validate={required}
+                        subscription={{
+                            value: true,
+                            active: true,
+                            error: true,
+                            touched: true
+                        }}
+                    >
+                        {
+                            ({ input, meta, placeholder }) => (
+
+                                <div
+                                    style={{
+                                        position: 'relative',
+                                        display: 'flex',
+                                        flexDirection: 'column',
+                                        alignItems: 'center',
+                                        background: meta.active ? '#ccffff' : 'transparent'
+                                    }}
+                                >
+
+                                    <TextField
+                                        {...input}
+                                        placeholder={placeholder}
+                                        label="Email"
+                                        type="email"
+                                        variant='outlined'
+                                        margin='dense'
+                                        required
+                                    />
+
+                                    {meta.error && meta.touched &&
+                                        <span
+                                            style={{
+                                                position: 'absolute',
+                                                right: '-40%',
+                                                top: '30%',
+                                                color: "red",
+                                                padding: '0 10px'
+                                            }}
+                                        >
+                                            <Typography variant='body1'>
+                                                {meta.error}
+                                            </Typography>
+                                        </span>
+                                    }
+
+                                </div>
+                            )
+                        }
+
+                    </Field>
+
+                </div>
+
+
+                <Button
+                    type='submit'
+                    variant='contained'
+                    color='primary'
+                    disabled={submitting}
+                    style={{
+                        margin: '10px 0 10px 0'
+                    }}
+                >
+                    Submit
+            </Button>
+
+                <FormSpy subscription={{ values: true }}>
+
+                    {({ values }) =>
+                        <pre
+                            style={{
+                                minHeight: '5rem',
+                                width: '100%',
+                                background: 'lightgrey',
+                                border: '5px solid grey'
+                            }}
+                        >
+                            {JSON.stringify(values, undefined, 2)}
+                        </pre>
+                    }
+
+                </FormSpy>
+
+            </form>
+        )}
+
+    />
+       
 )
 
 export default FinalForm
